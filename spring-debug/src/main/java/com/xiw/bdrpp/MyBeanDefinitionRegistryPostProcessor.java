0d40c7e7541f97f1e4c9aef7b7421d0aa35c48f3
童建伟 <tongjianwei@yunnk.com>
2022-06-02 11:06:28 +0800

    fix MyBeanDefinitionRegistryPostProcessor.java

--- spring-debug/src/main/java/com/xiw/bdrpp/MyBeanDefinitionRegistryPostProcessor.java
+++ spring-debug/src/main/java/com/xiw/bdrpp/MyBeanDefinitionRegistryPostProcessor.java
@@ -2,6 +2,8 @@ package com.xiw.bdrpp;
 
 import com.xiw.bean.Person;
 import org.springframework.beans.BeansException;
+import org.springframework.beans.PropertyValue;
+import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
 import org.springframework.beans.factory.support.AbstractBeanDefinition;
 import org.springframework.beans.factory.support.BeanDefinitionBuilder;
@@ -18,12 +20,18 @@ public class MyBeanDefinitionRegistryPostProcessor implements BeanDefinitionRegi
     @Override
     public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory) throws BeansException {
         System.out.println("MyBeanDefinitionRegistryPostProcessor.postProcessBeanFactory");
+        BeanDefinition myBean = beanFactory.getBeanDefinition("myBean");
+        PropertyValue name = myBean.getPropertyValues().getPropertyValue("name");
+        if(name != null) {
+            name.setConvertedValue("xiwang");
+        }
     }
 
     @Override
     public void postProcessBeanDefinitionRegistry(BeanDefinitionRegistry registry) throws BeansException {
         System.out.println("MyBeanDefinitionRegistryPostProcessor.postProcessBeanDefinitionRegistry");
         BeanDefinitionBuilder beanDefinitionBuilder = BeanDefinitionBuilder.genericBeanDefinition(Person.class);
+        beanDefinitionBuilder.addPropertyValue("name", "xiwang");
         AbstractBeanDefinition beanDefinition = beanDefinitionBuilder.getBeanDefinition();
         registry.registerBeanDefinition("myBean", beanDefinition);
     }
